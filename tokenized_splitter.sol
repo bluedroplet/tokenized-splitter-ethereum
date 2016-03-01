ea0f125d7390dce6bcc5e34855567118cdb4d796
Jonathan Brown <jbrown@bluedroplet.com>
2016-03-02 02:40:33 +0700

    Add comments. Re-arrange method order.

--- tokenized_splitter.sol
+++ tokenized_splitter.sol
@@ -1,3 +1,7 @@
+/**
+ * @title TokenizedSplitter
+ * @author Jonathan Brown <jbrown@bluedroplet.com>
+ */
 contract TokenizedSplitter {
 
     struct Account {
@@ -15,10 +19,73 @@ contract TokenizedSplitter {
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     function TokenizedSplitter(uint248 total) external {
+        // Store the total number of tokens.
         totalTokens = total;
+        // Give the contract creator all the tokens.
         accounts[msg.sender].activated = true;
         accounts[msg.sender].tokens = total;
         accountAddresses.push(msg.sender);
+    }
+
+    function allocateCash() {
+        // Calculate the amount of ether deposited into this contract since this
+        // function was last run.
+        uint unallocatedCash = this.balance - allocatedCash;
+        // If no ether needs to be allocated just return.
+        if (unallocatedCash == 0) {
+            return;
+        }
+        // Loop through all the token holders.
+        for (uint i = 0; i < accountAddresses.length; i++) {
+            // Get the storage address for this account.
+            Account account = accounts[accountAddresses[i]];
+            // Does this account have any tokens?
+            if (account.tokens != 0) {
+                // Increase the amount of ether this account by the amount of
+                // unallocated ether in proportion to how many of the tokens the
+                // account has. Integer division rounds down so a very small
+                // amount of ether may be lost.
+                account.cash += (unallocatedCash * account.tokens) / totalTokens;
+            }
+        }
+        // Set the record of allocated cash to the current balance.
+        allocatedCash = this.balance;
+    }
+
+    function cashout(address destination) external {
+        // Process any deposited ether.
+        allocateCash();
+        // Get the storage address for the sender's account.
+        Account account = accounts[msg.sender];
+        // Send the ether to the specified address.
+        destination.send(account.cash);
+        // Record this account as having no ether.
+        account.cash = 0;
+        // Set the record of allocated cash to the current balance.
+        allocatedCash = this.balance;
+    }
+
+    function transfer(address _to, uint256 _value) external returns (bool success) {
+        // Process any deposited ether.
+        allocateCash();
+        // Get the storage address for the sender and receiver's accounts.
+        Account fromAccount = accounts[msg.sender];
+        Account toAccount = accounts[_to];
+        // Check there is sufficent balance.
+        if (fromAccount.tokens < _value) {
+            return false;
+        }
+        // Ensure the receiver has an activated account.
+        if (toAccount.activated == false) {
+            toAccount.activated = true;
+            accountAddresses.push(_to);
+        }
+        // Update token balances.
+        fromAccount.tokens -= uint248(_value);
+        toAccount.tokens += uint248(_value);
+        // Log the event.
+        Transfer(msg.sender, _to, _value);
+        return true;
     }
 
     function totalSupply() constant external returns (uint256 supply) {
@@ -27,52 +94,6 @@ contract TokenizedSplitter {
 
     function balanceOf(address _owner) constant external returns (uint256 balance) {
         balance = accounts[_owner].tokens;
-    }
-
-    function allocateCash() {
-
-        uint unallocatedCash = this.balance - allocatedCash;
-
-        if (unallocatedCash == 0) {
-            return;
-        }
-
-        for (uint i = 0; i < accountAddresses.length; i++) {
-            Account account = accounts[accountAddresses[i]];
-            if (account.tokens != 0) {
-                // Rounds down. A very small amount of dust may be lost.
-                account.cash += (unallocatedCash * account.tokens) / totalTokens;
-            }
-        }
-
-        allocatedCash = this.balance;
-    }
-
-    function cashout(address destination) external {
-        allocateCash();
-        Account account = accounts[msg.sender];
-        uint amount = account.cash;
-        destination.send(amount);
-        allocatedCash -= amount;
-        account.cash = 0;
-    }
-
-    function transfer(address _to, uint256 _value) external returns (bool success) {
-        allocateCash();
-        Account fromAccount = accounts[msg.sender];
-        Account toAccount = accounts[_to];
-
-        if (fromAccount.tokens < _value) {
-            return false;
-        }
-        if (toAccount.activated == false) {
-            toAccount.activated = true;
-            accountAddresses.push(_to);
-        }
-        fromAccount.tokens -= uint248(_value);
-        toAccount.tokens += uint248(_value);
-        Transfer(msg.sender, _to, _value);
-        return true;
     }
 
 }
